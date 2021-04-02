c0250306dd5f8828af2160603323cfc537385b0e
Natanael <nata.mw@hotmail.com>
2021-04-02 18:50:22 -0300

    :sparkles: finished component header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Stack, Text } from "@chakra-ui/react";
+import { RiFileUserLine, RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 export function Header() {
   return (
     <Flex as="header" 
@@ -43,6 +43,36 @@ export function Header() {
           mr="4"
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex
+        aling="center"
+        ml="auto"
+      >
+        <HStack 
+          spacing="8" 
+          mx="8" 
+          pr="8" 
+          py="1" 
+          color="gray.300" 
+          borderRightWidth={1} 
+          borderColor="gray.700" 
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>Natanael</Text>
+            <Text color="gray.300" fontSize="small">nata.mw@hotmail.com</Text>
+          </Box>
+
+          <Avatar size="md" name="Natanael Silva" src="https://github.com/Nata07.png" />
+
+        </Flex>
       </Flex>
     </Flex>
   )
